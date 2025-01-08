1f5cf5547f43ea5db014b854038707c90f473651
HughZhang <40616309+HughZhang98@users.noreply.github.com>
2025-01-08 15:17:35 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,32 @@
-const http = require ('http');
-let times = 1
+const express = require("express");
+const app = express();
 
-http.createServer((req, res) => {
-    res.writeHead(200, { 
-        'Content-Type': 'text/event-stream; charset=UTF-8',
-        'Cache-Control': 'no-cache',
-        'Connection': 'keep-alive',
-        'Access-Control-Allow-Origin': 'http://localhost:3000',
-        'Access-Control-Allow-Credentials': 'true'
-    });
+let messageCount = 1;
 
-    const interval = setInterval(() => {
-        res.write("event: message\n");
-        res.write("data: " + `测试消息${times},` + "\n\n");
-        times++;
-    }, 5000);
-    
-    req.on('close', () => {
-        clearInterval(interval);
-        console.log('Client disconnected');
-    });
-}).listen(3003, () => {
-    console.log('SSE Server is running on http://localhost:3003');
-})
+const corsOptions = {
+  origin: "http://localhost:3003",
+  credentials: true,
+};
+
+app.use(require("cors")(corsOptions));
+
+app.get("/", (req, res) => {
+  res.writeHead(200, {
+    "Content-Type": "text/event-stream",
+    "Cache-Control": "no-cache",
+    Connection: "keep-alive",
+  });
+
+  const interval = setInterval(() => {
+    res.write(`event: message\n`);
+    res.write(`data: 测试消息${messageCount++}，\n\n`);
+  }, 5000);
+  req.on("close", () => {
+    clearInterval(interval);
+    console.log("Client disconnected");
+  });
+});
+const PORT = process.env.PORT || 3003;
+app.listen(PORT, () => {
+  console.log(`SSE Server is running on http://localhost:${PORT}`);
+});
